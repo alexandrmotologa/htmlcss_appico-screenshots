b5a907512d595ed0b7c0b053e2238d44973f80ea
alexandrmotologa <al.motologa@gmail.com>
2020-12-14 23:47:16 +0200

    hw

--- screenshots.js
+++ screenshots.js
@@ -4,6 +4,29 @@ $(document).ready(function(){
     infinite: true,
     arrows: true,
     dots: true,
-
+    responsive: [
+      {
+        breakpoint: 1140,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            infinite: true,
+        }
+      },
+      {
+        breakpoint: 940,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+            arrows: false,
+        }
+      }
+    ]
   });
 });
+
+AOS.init();
